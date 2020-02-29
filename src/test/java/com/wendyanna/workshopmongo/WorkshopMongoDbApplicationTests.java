9cf012e87767a1944f5c51d6d1c8d38fb63f1192
Wendy-Anna <wendyufpb@gmail.com>
2020-02-29 10:49:33 -0300

    Primeiro commit

---
+++ src/test/java/com/wendyanna/workshopmongo/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendyanna.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
