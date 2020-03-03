9ddf54647c5bdd80d76914870883c7c2fcd47fb2
Wendy-Anna <wendyufpb@gmail.com>
2020-03-03 07:27:45 -0300

    Consulta simples com @Query

--- src/main/java/com/wendyanna/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wendyanna/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.wendyanna.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wendyanna.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.wendyanna.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
